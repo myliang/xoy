b0d3a3aec9c4d85c8df2a4bffe63467139325461
yuliang.liang <yuliang.liang@qunar.com>
2014-04-25 19:59:24 +0800

    one line code

--- src/torrent.c
+++ src/torrent.c
@@ -10,13 +10,15 @@ torrent* torrent_init (b_encode* bp) {
   if(NULL == bp) return NULL;
   torrent* tt = malloc(sizeof(torrent));
   b_dict* bd = bp->data.dpv;
-  printf("type=%d\n", bp->type);
+  // printf("type=%d\n", bp->type);
   while(NULL != bd) {
     printf("key=%s\n", bd->key);
     unsigned int key_len = strlen(bd->key);
     int max_len = max(key_len, 13);
     if(strncmp("announce-list", bd->key, max_len) == 0) {
       b_list* bl = bd->value->data.lpv;
+      torrent_tracker head;
+      torrent_tracker* tp = &head;
       while(NULL != bl) {
         b_list* bll = bl->item->data.lpv;
         printf("%s\n", bll->item->data.cpv);
